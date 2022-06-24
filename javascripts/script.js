f8b428469a347aca45a8baf771319a6d87dbe929
thevictorwhyte <victordavidwhyte@gmail.com>
2022-06-24 21:01:55 +0100

    feat: broadcast startGame event and choose referee

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,7 @@
-// Canvas Related 
-const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
-const socket = io('http://localhost:3000')
+// Canvas Related
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+const socket = io("http://localhost:3000");
 let paddleIndex = 0;
 
 let width = 500;
@@ -30,7 +30,7 @@ let score = [0, 0];
 
 // Create Canvas Element
 function createCanvas() {
-  canvas.id = 'canvas';
+  canvas.id = "canvas";
   canvas.width = width;
   canvas.height = height;
   document.body.appendChild(canvas);
@@ -40,23 +40,23 @@ function createCanvas() {
 // Wait for Opponents
 function renderIntro() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
 
   // Intro Text
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.font = "32px Courier New";
-  context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
+  context.fillText("Waiting for opponent...", 20, canvas.height / 2 - 30);
 }
 
 // Render Everything on Canvas
 function renderCanvas() {
   // Canvas Background
-  context.fillStyle = 'black';
+  context.fillStyle = "black";
   context.fillRect(0, 0, width, height);
 
   // Paddle Color
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
 
   // Bottom Paddle
   context.fillRect(paddleX[0], height - 20, paddleWidth, paddleHeight);
@@ -69,19 +69,19 @@ function renderCanvas() {
   context.setLineDash([4]);
   context.moveTo(0, 350);
   context.lineTo(500, 350);
-  context.strokeStyle = 'grey';
+  context.strokeStyle = "grey";
   context.stroke();
 
   // Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  context.fillStyle = 'white';
+  context.fillStyle = "white";
   context.fill();
 
   // Score
   context.font = "32px Courier New";
-  context.fillText(score[0], 20, (canvas.height / 2) + 50);
-  context.fillText(score[1], 20, (canvas.height / 2) - 30);
+  context.fillText(score[0], 20, canvas.height / 2 + 50);
+  context.fillText(score[1], 20, canvas.height / 2 - 30);
 }
 
 // Reset Ball to Center
@@ -146,7 +146,6 @@ function ballBoundaries() {
       trajectoryX[1] = ballX - (paddleX[1] + paddleDiff);
       speedX = trajectoryX[1] * 0.3;
     } else {
-
       ballReset();
       score[0]++;
     }
@@ -165,22 +164,37 @@ function animate() {
 function startGame() {
   createCanvas();
   renderIntro();
+  socket.emit("ready");
 
   paddleIndex = 0;
   window.requestAnimationFrame(animate);
-  canvas.addEventListener('mousemove', (e) => {
+  canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
     paddleX[paddleIndex] = e.offsetX;
     if (paddleX[paddleIndex] < 0) {
       paddleX[paddleIndex] = 0;
     }
-    if (paddleX[paddleIndex] > (width - paddleWidth)) {
+    if (paddleX[paddleIndex] > width - paddleWidth) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
     // Hide Cursor
-    canvas.style.cursor = 'none';
+    canvas.style.cursor = "none";
   });
 }
 
+let readyPlayerCount = 0;
 // On Load
 startGame();
+socket.on("connect", () => {
+  console.log("Connected as...", socket.id);
+
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      io.emit("startGame", socket.id);
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 const PORT = 3000;
 server.listen(PORT);
-console.log(`Listening on Port ${PORT}`)
+console.log(`Listening on Port ${PORT}`);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-
-})
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
